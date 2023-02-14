8da7710a7f8e5d7c562642d8b7ac99f5618b096c
Otto Bolyós <obolyos@mriiot.com>
2023-02-14 21:33:01 +0100

    feat(gateway-e2e): update `gateway-e2e`
    - Update tests.
    - Remove a duplicate word in a comment in `apps/gateway-e2e/src/support/global-setup.ts`.
    - Use environment variables in `apps/gateway-e2e/src/support/test-setup.ts` (albeit without `@nestjs/config`).

--- apps/gateway-e2e/src/support/test-setup.ts
+++ apps/gateway-e2e/src/support/test-setup.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 module.exports = async function () {
 	// Configure axios for tests to use.
-	const host = process.env.HOST ?? 'localhost'
-	const port = process.env.PORT ?? '3000'
+	const host = process.env.L99_GW_HOST ?? '127.0.0.1'
+	const port = process.env.L99_GW_PORT ?? 3333
 	axios.defaults.baseURL = `http://${host}:${port}`
 }
